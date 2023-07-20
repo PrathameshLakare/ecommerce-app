90f9b68463b8be9e3602d0630ef29948aa39187c
PrathameshLakare <prathameshlakare001@gmail.com>
2023-07-20 14:29:06 +0530

    2-Cart added

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,11 @@
+import Singup from "../features/auth/components/Signup";
+
+function SingupPage() {
+  return (
+    <div>
+      <Singup></Singup>
+    </div>
+  );
+}
+
+export default SingupPage;
